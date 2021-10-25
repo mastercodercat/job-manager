23b141e010d2591b7f4ba149ef61a330789723bf
mastercodercat <mastercoderoy64@gmail.com>
2021-10-25 00:24:22 -0700

    added: react-toast and divided screen to 4 sections

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import loadable from "@loadable/component";
+import Grid from "@mui/material/Grid";
 
 import "./App.css";
 
 const Layout = loadable(() => import("./components/Layout"));
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          Create a job form
+        </Grid>
+        <Grid item xs={6}>
+          Job detail
+        </Grid>
+        <Grid item xs={4}>
+          Most used skills
+        </Grid>
+        <Grid item xs={8}>
+          Job board
+        </Grid>
+      </Grid>
+      <ToastContainer position="top-right" />
+    </Layout>
+  );
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
