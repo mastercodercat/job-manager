7e0146beb72ca1279a79ab7859505d32268f060f
mastercodercat <mastercoderoy64@gmail.com>
2021-10-25 00:40:39 -0700

    added: axios api

---
+++ frontend/src/service/api.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance } from "axios";
+import { toast } from "react-toastify";
+
+import { Job } from "../redux/modules/job/types";
+import { Skill } from "../redux/modules/skill/types";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_API_URL,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const msg = error.response?.data?.msg || "Failed to connect to server";
+    toast.error(msg);
+  }
+);
+
+const api = {
+  createJob: (job: Job) => axiosInstance.post<Required<Job>>("/jobs/", job),
+};
+
+export default api;
